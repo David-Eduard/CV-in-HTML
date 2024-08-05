951c673af591a0d3b4cebdcef6b6053492ac0e0e
David-Eduard <melintedavid20@yahoo.com>
2024-08-05 20:11:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function displayDate() {
+    document.getElementById('demo').innerHTML = Date()
+}
+function background() {
+    document.body.style.backgroundColor = "#f3f3f3";
+    document.body.style.backgroundImage = "url(tree.jpg)";
+} 
